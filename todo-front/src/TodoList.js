50f6dfe7125d03c2f9498a59ce0b054c6d2fdaab
MaciejReimann <maciej.reimann@gmail.com>
2018-07-29 15:34:16 +0200

    Initial commit; basic setup

---
+++ todo-front/src/TodoList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    }
+  };
+
+  componentWillMount() {
+    fetch('/api/todos')
+    .then(data => data.json())
+    .then(todos => this.setState({todos}))
+  }
+
+  render() {
+    return (
+        <h1>Todo List!</h1>
+    )
+  }
+}
+
+export default TodoList;
