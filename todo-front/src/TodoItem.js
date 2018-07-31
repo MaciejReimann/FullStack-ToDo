d5734a5eee3439bd593dae1f5af032f5b2527f1b
MaciejReimann <maciej.reimann@gmail.com>
2018-07-31 12:26:01 +0200

    todo onclick toggle added

--- todo-front/src/TodoItem.js
+++ todo-front/src/TodoItem.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const todoItem = ({name}) => (
+const todoItem = ({ name, completed, onDelete, onToggle}) => (
   <li>
+    <span
+      style = {{ textDecoration: completed ? 'line-through' : 'none'}}
+      onClick = {onToggle}
+    >
     {name}
+    </span>
+    <span onClick = {onDelete}> X </span>
   </li>
 );
 
